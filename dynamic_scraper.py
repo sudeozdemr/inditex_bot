a7d7a091835f31b0b3fab5a9579b7decb144b6fc
Sude ÖZDEMİR <84103785+sudeozdemr@users.noreply.github.com>
2025-01-28 14:34:50 +0300

    Python script with selenium.

---
+++ dynamic_scraper.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+# Geckodriver yolunu belirtin
+gecko_driver_path = '/usr/local/bin/geckodriver'  # Geckodriver yolunu buraya yazın
+
+# Geckodriver servisini başlat
+service = Service(gecko_driver_path)
+driver = webdriver.Firefox(service=service)
+
+# Hedef web sayfasına git
+driver.get('https://www.zara.com/tr/tr/yumusak-dokulu-oversize-kaban-p03046037.html?v1=434384588&v2=2419033')
+
+# Sayfadaki başlığı çek
+title = driver.find_element(By.CSS_SELECTOR, 'h1').text
+print(title)
+
+
+# Tarayıcıyı kapat
+driver.quit()
